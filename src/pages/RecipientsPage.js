b1eb8a237ac367260b6bda4df29b054204362f81
Rayeed _ <_rayeed@live.com>
2023-01-07 15:45:58 +0000

    add navigation to household page from recipients

--- src/pages/RecipientsPage.js
+++ src/pages/RecipientsPage.js
@@ -1,5 +1,15 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionIcon,
+  AccordionPanel,
+  AccordionButton,
+  Box,
+  Text,
+} from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link, Outlet } from "react-router-dom";
 import Table from "../components/TableWrapper";
 import AddRecipient from "../components/AddRecipient";
 import Layout from "../components/Layout";
@@ -7,7 +17,7 @@ import Layout from "../components/Layout";
 const baseURL = `${process.env.REACT_APP_API_URL}/households`;
 
 export default function RecipientsPage() {
-  const [recipients, setRecipients] = useState([]);
+  const [households, setHouseholds] = useState([]);
 
   useEffect(() => {
     // this axios call can be a hook?
@@ -15,17 +25,76 @@ export default function RecipientsPage() {
     axios
       .get(baseURL)
       .then((response) => {
-        if (response.data.length > 0) setRecipients(response.data);
+        if (response.data.length > 0) {
+          setHouseholds(response.data);
+        }
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
 
+  const tableRowData = [];
+  if (households.length > 0) {
+    households.forEach(({ members, ...rest }) => {
+      members.forEach((member) => {
+        tableRowData.push({ ...member, ...rest });
+      });
+    });
+  }
+
+  const dataTransform = (recipientsData) =>
+    recipientsData.map(
+      ({
+        name,
+        householdId,
+        adultCount,
+        childCount,
+        ...remainingProperties
+      }) => {
+        return {
+          name: (
+            <Link
+              to={`/households/${householdId}`}
+              state={{
+                selectedRecipientName: name,
+                ...households.find((house) => house.householdId == householdId),
+              }}
+            >
+              {name}
+            </Link>
+          ),
+          ...remainingProperties,
+          "Number of family members": (
+            <Accordion allowToggle borderColor="white">
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Box as="span" flex="1" textAlign="left">
+                      {adultCount + childCount}
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  <Text>Number of adults: {adultCount}</Text>
+                  <Text>Number of children: {childCount}</Text>
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+          ),
+        };
+      }
+    );
+  // make data transform a pure function
+
+  // table needs a group by household button
+  // table needs an ability to search
   return (
     <Layout title="Here be yer recipients">
       <AddRecipient />
-      <Table data={recipients} />
+      <Table data={tableRowData} dataTransform={dataTransform} />
+      <Outlet />
     </Layout>
   );
 }
