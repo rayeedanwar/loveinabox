689418d058bfc2ea49ad986c8a0623152b259fa6
Rayeed _ <_rayeed@live.com>
2023-01-11 10:37:22 +0000

    make title optional in Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,14 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 
 export default function Layout({ title, children }) {
   return (
-    <div>
-      <Heading size="3xl" margin="5%" padding="5%">
-        {title}
-      </Heading>
+    <Box width="80vw" margin="auto" padding="3%">
+      {title && (
+        <Heading size="3xl" margin="5%" padding="5%">
+          {title}
+        </Heading>
+      )}
       {children}
-    </div>
+    </Box>
   );
 }
